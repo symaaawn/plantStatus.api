48016057d01492a342e7079bdc22bdaaf3a5defd
Simon <simon.neugebauer@gmail.com>
2019-03-24 12:20:25 +0100

    [ADD] added post method to ttn controller

--- plantStatus.api/Controllers/TheThingsNetworkController.cs
+++ plantStatus.api/Controllers/TheThingsNetworkController.cs
@@ -4,12 +4,41 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NLog;
+using plantStatus.api.Entities;
+using plantStatus.api.Models;
+using plantStatus.api.Services;
 
 namespace plantStatus.api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/ttn")]
     [ApiController]
     public class TheThingsNetworkController : ControllerBase
     {
+        private ISensorInfoRepository _sensorInfoRepository;
+        private static Logger _log = LogManager.GetCurrentClassLogger();
+
+        public TheThingsNetworkController(ISensorInfoRepository repository) {
+            _sensorInfoRepository = repository;
+        }
+
+        [HttpPost]
+        public IActionResult CreateSensorData([FromBody] TheThingsNetworkBodyDto ttnBody)
+        {
+            Sensor sensor = _sensorInfoRepository.GetSensor(ttnBody.dev_id, false);
+
+            if (sensor == null)
+            {
+                _sensorInfoRepository.AddSensor(new Sensor
+                {
+                    Description = ttnBody.dev_id
+                });
+                if (!_sensorInfoRepository.Save()) 
+                {
+                    return StatusCode(500, "our server did an oopsie");
+                }
+            }
+            return Ok();
+        }
     }
 }
